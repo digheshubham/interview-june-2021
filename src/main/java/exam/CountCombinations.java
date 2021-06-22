2a915c756beefe19c17f24bde9b336917cae42d9
Shubham Dighe <86230406+digheshubham@users.noreply.github.com>
2021-06-22 12:53:39 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -4,6 +4,8 @@ public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    int a=(total*(total-1))/(2*(k-1));
+    System.out.println(a);
     return 0;
   }
 
