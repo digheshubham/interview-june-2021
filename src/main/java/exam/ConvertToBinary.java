4a080158eeb3f0db3ba4425e2a0c1271c9386e49
Shubham Dighe <86230406+digheshubham@users.noreply.github.com>
2021-06-22 12:50:59 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,22 @@
 package exam;
 
+
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    int a[]=new a[10];
+    
+  for(int i=0;n>0;i++){
+    a[i]=n%2;
+    n=n/2;
   }
+    
+    for(int i=i-1;i>=0;i--)
+        {
+          System.out.println(a[i]);
+    }
+    
+            return "";
+  
 }
